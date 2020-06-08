dd669ca0ed64c619b53a037f6e583873b9a19567
j.divol <jeandi7@gmail.com>
2020-06-08 19:44:28 +0200

    initial input

---
+++ lestuples.py
@@ -0,0 +1,18 @@
+# Un tuple est une sequence immuable : on ne peut pas modiﬁer ses elements  ni lui en ajouter ou lui en enlever
+
+t = 2, 2.0, 'deux', "deux",True, (1, 2, 3, 4)
+
+print(t)
+
+print(t[3])
+print(t[0])
+print(t[-1])
+
+point = 3,4
+
+print(point)
+
+x,y = point
+
+print (x)
+print (y)
